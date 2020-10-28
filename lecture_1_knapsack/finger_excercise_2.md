3afd284e6752fd8d7ce8380923fc6afccc23787e
Achintya Gupta <ag.ds.bubble@gmail.com>
2020-10-28 12:10:48 +0530

    Add Finger Excercise 2

---
+++ lecture_1_knapsack/finger_excercise_2.md
@@ -0,0 +1,85 @@
+### Please help the burglar out! For each of the following greedy metrics, what should be the burglar's first two choices of items? Here's a table of the items from the slides:
+
+|   Item   |     $    |   kg  |
+| :------: | :------: | :---: |
+| Clock    |   175    | 10    |
+| Picture  |   90     | 9     |
+| Radio    |   20     | 4     |
+| Vase     |   50     | 2     |
+| Book     |   10     | 1     |
+| Computer |   200    | 20    |
+
+### For this problem, assume that the maximum weight the burglar can carry is 20.
+
+Q1)
+
+    Metric: max value
+
+### Q1.1) The burglar should first pick:
+
+- [ ] Clock
+- [ ] Picture
+- [ ] Radio
+- [ ] Vase
+- [ ] Book
+- [x] Computer
+- [ ] No more space
+
+### Q1.2) and should next pick:
+
+- [ ] Clock
+- [ ] Picture
+- [ ] Radio
+- [ ] Vase
+- [ ] Book
+- [ ] Computer
+- [x] No more space
+
+Q2)
+
+    Metric: min weight
+
+### Q2.1) The burglar should first pick:
+
+- [ ] Clock
+- [ ] Picture
+- [ ] Radio
+- [ ] Vase
+- [x] Book
+- [ ] Computer
+- [ ] No more space
+
+### Q2.2) and should next pick:
+
+- [ ] Clock
+- [ ] Picture
+- [ ] Radio
+- [x] Vase
+- [ ] Book
+- [ ] Computer
+- [ ] No more space
+
+Q3)
+
+    Metric: max value/weight ratio
+
+### Q3.1) The burglar should first pick:
+
+- [ ] Clock
+- [ ] Picture
+- [ ] Radio
+- [x] Vase
+- [ ] Book
+- [ ] Computer
+- [ ] No more space
+
+### Q3.2) and should next pick:
+
+- [x] Clock
+- [ ] Picture
+- [ ] Radio
+- [ ] Vase
+- [ ] Book
+- [ ] Computer
+- [ ] No more space
+
--- lecture_1_knapsack/finger_excercise_2.py
+++ lecture_1_knapsack/finger_excercise_2.py
@@ -1,33 +1,33 @@
-class Food:
+class Item:
     def __init__(self, n, v, c):
         self.name = n
         self.value = v
-        self.calories = c
+        self.weight = c
 
     def __str__(self):
-        return self.name + f': <V:{self.value}, C:{self.calories}>'
+        return self.name + f': <V:{self.value}, C:{self.weight}>'
 
     def __repr__(self):
-        return self.name + f': <V:{self.value}, C:{self.calories}>'
+        return self.name + f': <V:{self.value}, C:{self.weight}>'
     
     def getValue(self):
         return self.value
 
     def getCost(self):
-        return self.calories
+        return self.weight
 
     def getDensity(self):
         return self.getValue()/self.getCost()
 
-def pprint_foodm(fmlist):
+def pprint_item(fmlist):
     for k in fmlist:
         print('\t', k.__str__())
 
 # Building Menu
-zippack = zip(['Wine', 'Beer', 'Pizza', 'Burger', 'Fries', 'Coke', 'Apple', 'Donut'],
-              [89, 90, 30, 50, 90, 79, 90, 10],
-              [123, 154, 258, 354, 365, 150, 95, 195])
-food_menu = [Food(n,v,c) for n,v,c in zippack]
+zippack = zip(['Clock', 'Picture', 'Radio', 'Vase', 'Book', 'Computer'],
+              [175, 90, 20, 50, 10, 200],
+              [10, 9,4, 2, 1, 20])
+all_items = [Item(n,v,w) for n,v,w in zippack]
 # pprint_foodm(food_menu)
 
 def greedy(items, maxCost, keyFunction, keyFuncName):
@@ -39,19 +39,19 @@ def greedy(items, maxCost, keyFunction, keyFuncName):
             optimal_selection.append(itemsCopy[i])
             totalValue+=itemsCopy[i].getValue()
             totalCost+=itemsCopy[i].getCost()
-    print(f'For maxCost @ {maxCost} & keyFunc @ {keyFuncName} . Total Value : {totalValue} Total Calories : {totalCost}')
-    pprint_foodm(optimal_selection)
+    print(f'For maxCost @ {maxCost} & keyFunc @ {keyFuncName} . Total Value : {totalValue} Total weight : {totalCost}')
+    pprint_item(optimal_selection)
     print('\n')
 
 def test_greedys(max_cals):
     # By Value : Higher the Value the better
-    greedy(food_menu, max_cals, lambda x : x.getValue(), 'By Value')
+    greedy(all_items, max_cals, lambda x : x.getValue(), 'By Value')
 
-    # By Cost : Lower the calories the better
-    greedy(food_menu, max_cals, lambda x : 1/x.getCost(), 'By Cost')
+    # By Cost : Lower the weight the better
+    greedy(all_items, max_cals, lambda x : 1/x.getCost(), 'By Weight')
 
     # By Density : Higher the Value/Cost the better
-    greedy(food_menu, max_cals, lambda x : x.getDensity(), 'By Density')
+    greedy(all_items, max_cals, lambda x : x.getDensity(), 'By Density')
     
 
-test_greedys(1000)
+test_greedys(20)
